3ab25d50ef30269774aa96cf8a326de25e58fd9d
brain.exe <mariovionikos@gmail.com>
2024-06-04 19:25:05 +0300

    gdfh

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,6 +2,11 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +14,10 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("minehearth");
+	public static final Item CUSTOM_ITEM =
+      Registry.register(Registries.ITEM, new Identifier("minehearth", "custom_item"),
+        new Item(new Item.Settings()));
 
 	@Override
 	public void onInitialize() {
