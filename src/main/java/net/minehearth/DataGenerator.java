9d80a33cc31961df18777aaeb4bef20b4a8b38b9
brain.exe <mariovionikos@gmail.com>
2024-06-07 00:46:41 +0300

    Added Data Generator, Fixed paths

---
+++ src/main/java/net/minehearth/DataGenerator.java
@@ -0,0 +1,15 @@
+package net.minehearth;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator g) {
+        FabricDataGenerator.Pack pack = g.createPack();
+
+
+    }
+
+
+}
--- src/main/java/net/minehearth/Minehearth.java
+++ src/main/java/net/minehearth/Minehearth.java
@@ -1,23 +1,21 @@
-package com.example;
+package net.minehearth;
 
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Minehearth implements ModInitializer {
+	public static final String MODID = "minehearth";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("minehearth");
-	public static final Item CUSTOM_ITEM =
-      Registry.register(Registries.ITEM, new Identifier("minehearth", "custom_item"),
-        new Item(new Item.Settings()));
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final Item CUSTOM_ITEM = new Item(new Item.Settings());
 
 	@Override
 	public void onInitialize() {
@@ -26,5 +24,11 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM, loc("custom_item"), CUSTOM_ITEM);
+	}
+
+	public static Identifier loc(String path) {
+		return new Identifier(MODID, path);
 	}
 }
